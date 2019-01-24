2340b11bdd6e7a8ed97d8b31dd6abf904a2c77ec
jsh854 <junaidshah349@gmail.com>
2019-01-24 10:30:44 +0530

    modified script for better

--- scriptforbackground.js
+++ scriptforbackground.js
@@ -7,23 +7,33 @@ let tex = document.querySelector("h3");
 let rght = document.querySelectorAll("button")[0];
 let left = document.querySelectorAll("button")[1];
 let copyte = document.getElementsByClassName("clicktocopy")[0];
+let copied = false;
+let addcheck = false;
+
+
 copyte.classList.add("e");
 
 //functions 
 function addremove(){
+	addcheck = true;
     copyte.classList.remove("e");
     copyte.classList.add("di");
 }
 function change1(){
     bdy.style.background="linear-gradient(to right,"+color1.value+","+color2.value +")";
     tex.textContent=bdy.style.background +";";
-    addremove();
+    if(copied){copyte.innerHTML = "click to copy";}
+    if(!addcheck){ addremove(); }
+   
+    
  
 }
 function change2(){
     bdy.style.background="linear-gradient(to left,"+color1.value+","+color2.value +")";
     tex.textContent=bdy.style.background +";";
-    addremove();
+     if(copied){copyte.innerHTML = "click to copy";}
+   	if(!addcheck){ addremove(); }
+   
     
 }
 
@@ -36,8 +46,7 @@ function copytext(){
       document.execCommand('copy');
       document.body.removeChild(el);
       copyte.innerHTML = "copied";
-      
-      
+      copied = true;     
 }
 
 
